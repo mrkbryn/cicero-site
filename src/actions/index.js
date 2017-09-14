6d3c29db46e03c5fb110143b5cef2f1ee1027624
Mark Bryan <mab539@cornell.edu>
2017-09-13 21:00:53 -0400

    configure action creators and reducers for api call

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,55 +1,87 @@
-export const SET_ALGORITHM = 'SET_ALGORITHM';
+import fetch from 'isomorphic-fetch'
+
+export const SET_ALGORITHM = 'SET_ALGORITHM'
 export const setAlgorithm = algorithm => {
     return {
         type: SET_ALGORITHM,
         algorithm
-    };
-};
+    }
+}
 
-export const SET_DATA_INPUT_METHOD = 'SET_DATA_INPUT_METHOD';
+export const SET_DATA_INPUT_METHOD = 'SET_DATA_INPUT_METHOD'
 export const setDataInputMethod = inputMethod => {
     return {
         type: SET_DATA_INPUT_METHOD,
         inputMethod
-    };
-};
+    }
+}
 
-export const SET_MAX_ALLOWABLE_CONTEXT_SIZE = 'SET_MAX_ALLOWABLE_CONTEXT_SIZE';
+export const SET_MAX_ALLOWABLE_CONTEXT_SIZE = 'SET_MAX_ALLOWABLE_CONTEXT_SIZE'
 export const setMaxAllowableContextSize = mS => {
     return {
         type: SET_MAX_ALLOWABLE_CONTEXT_SIZE,
         mS
-    };
-};
+    }
+}
 
-export const SET_MAX_ALLOWABLE_NUMERICAL_DOMAIN_WIDTH = 'SET_MAX_ALLOWABLE_NUMERICAL_DOMAIN_WIDTH';
+export const SET_MAX_ALLOWABLE_NUMERICAL_DOMAIN_WIDTH = 'SET_MAX_ALLOWABLE_NUMERICAL_DOMAIN_WIDTH'
 export const setMaxAllowableNumericalDomainWidth = mW => {
     return {
         type: SET_MAX_ALLOWABLE_NUMERICAL_DOMAIN_WIDTH,
         mW
-    };
-};
+    }
+}
 
-export const SET_MAX_ALLOWABLE_CATEGORICAL_DOMAIN_SIZE = 'SET_MAX_ALLOWABLE_CATEGORICAL_DOMAIN_SIZE';
+export const SET_MAX_ALLOWABLE_CATEGORICAL_DOMAIN_SIZE = 'SET_MAX_ALLOWABLE_CATEGORICAL_DOMAIN_SIZE'
 export const setMaxAllowableCategoricalDomainSize = mC => {
     return {
         type: SET_MAX_ALLOWABLE_CATEGORICAL_DOMAIN_SIZE,
         mC
-    };
-};
+    }
+}
 
-export const SET_TIMEOUT = 'SET_TIMEOUT';
+export const SET_TIMEOUT = 'SET_TIMEOUT'
 export const setTimeout = timeout => {
     return {
         type: SET_TIMEOUT,
         timeout
-    };
-};
+    }
+}
 
-export const SET_EPSILON = 'SET_EPSILON';
+export const SET_EPSILON = 'SET_EPSILON'
 export const setEpsilon = epsilon => {
     return {
         type: SET_EPSILON,
         epsilon
-    };
-};
+    }
+}
+
+/**
+ * Dispatches actions as API call progresses
+ */
+export const fetchTestResult = (testInstance) => {
+    return dispatch => {
+        dispatch(requestTestResult(testInstance))
+        // TODO: fetch from database when implemented
+        // return fetch(`https://www.api.com/test`)
+        //     .then(response => response.json())
+        //     .then(json => dispatch(receiveTestResult(json)))
+        dispatch(receiveTestResult({}))
+    }
+}
+
+export const REQUEST_TEST_RESULT = 'REQUEST_TEST_RESULT'
+export const requestTestResult = (testInstance) => {
+    return {
+        type: REQUEST_TEST_RESULT,
+        testInstance
+    }
+}
+
+export const RECEIVE_TEST_RESULT = 'RECEIVE_TEST_RESULT'
+export const receiveTestResult = (testResult) => {
+    return {
+        type: RECEIVE_TEST_RESULT,
+        testResult
+    }
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from 'redux'
 import {
     SET_ALGORITHM,
     SET_DATA_INPUT_METHOD,
@@ -7,70 +7,91 @@ import {
     SET_MAX_ALLOWABLE_CATEGORICAL_DOMAIN_SIZE,
     SET_TIMEOUT,
     SET_EPSILON,
-} from '../actions';
+    REQUEST_TEST_RESULT,
+    RECEIVE_TEST_RESULT,
+} from '../actions'
 
 const algorithm = (state = 'naive', action) => {
     switch (action.type) {
         case SET_ALGORITHM:
-            return action.algorithm;
+            return action.algorithm
         default:
-            return state;
+            return state
     }
-};
+}
 
 const maxAllowableContextSize = (state = 2, action) => {
     switch (action.type) {
         case SET_MAX_ALLOWABLE_CONTEXT_SIZE:
-            return action.mS;
+            return action.mS
         default:
-            return state;
+            return state
     }
-};
+}
 
 const maxAllowableNumericalDomainWidth = (state = 2.0, action) => {
     switch (action.type) {
         case SET_MAX_ALLOWABLE_NUMERICAL_DOMAIN_WIDTH:
-            return action.mW;
+            return action.mW
         default:
-            return state;
+            return state
     }
-};
+}
 
 const maxAllowableCategoricalDomainSize = (state = 2, action) => {
     switch (action.type) {
         case SET_MAX_ALLOWABLE_CATEGORICAL_DOMAIN_SIZE:
-            return action.mC;
+            return action.mC
         default:
-            return state;
+            return state
     }
-};
+}
 
-const timeout = (state = 120, action) => {
+const timeout = (state = 5, action) => {
     switch (action.type) {
         case SET_TIMEOUT:
-            return action.timeout;
+            return action.timeout
         default:
-            return state;
+            return state
     }
-};
+}
 
 const epsilon = (state = 0.1, action) => {
     switch (action.type) {
         case SET_EPSILON:
-            return action.epsilon;
+            return action.epsilon
         default:
-            return state;
+            return state
     }
-};
+}
 
 const dataInputMethod = (state = 'sqlQuery', action) => {
     switch (action.type) {
         case SET_DATA_INPUT_METHOD:
-            return action.inputMethod;
+            return action.inputMethod
         default:
-            return state;
+            return state
     }
-};
+}
+
+const testResultFetch = (state = {
+    isFetching: false,
+    testResult: {}
+}, action) => {
+    switch (action.type) {
+        case REQUEST_TEST_RESULT:
+            return {
+                isFetching: true
+            }
+        case RECEIVE_TEST_RESULT:
+            return Object.assign({}, state, {
+                isFetching: false,
+                testResult: action.testResult
+            })
+        default:
+            return state
+    }
+}
 
 const ciceroSite = combineReducers({
     algorithm,
@@ -80,6 +101,7 @@ const ciceroSite = combineReducers({
     timeout,
     epsilon,
     dataInputMethod,
-});
+    testResultFetch,
+})
 
-export default ciceroSite;
+export default ciceroSite
